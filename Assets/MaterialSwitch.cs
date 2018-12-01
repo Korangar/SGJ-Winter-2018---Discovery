63ce7349f07010a4916ba7c64eaa0fca5900efc6
XMG-II\fuhax <ilja.immler@gmail.com>
2018-12-02 00:02:40 +0100

    Tweaking

---
+++ Assets/MaterialSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwitch : MonoBehaviour
+{
+    public Vector2 texOffset;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SkinnedMeshRenderer mr = GetComponent<SkinnedMeshRenderer>();
+        mr.material.mainTextureOffset = texOffset;
+    }
+}
