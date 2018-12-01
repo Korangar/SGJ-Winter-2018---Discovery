65ce0507a2e09bf5c981f4a9a5e8cfbf3a4e50fc
XMG-II\fuhax <ilja.immler@gmail.com>
2018-12-01 22:28:49 +0100

    PP Fixed

---
+++ Assets/SceneLoad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public string level;
+    public LoadSceneMode loadMode;
+
+    private void OnEnable()
+    {
+        SceneManager.LoadScene(level, loadMode);
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.UnloadSceneAsync(level);
+    }
+}
