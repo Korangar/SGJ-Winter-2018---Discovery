f9b779ca6a9973620522ddf47e2522e8c77cad79
Taraxippus <goesjakob@gmail.com>
2018-12-01 12:41:53 +0100

    Player Movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Controller input;
+    public float movementSpeed = 1;
+
+    private NavMeshAgent agent;
+
+    public enum Controller
     {
-        
+        Controller1,
+        Controller2,
+        Controller3,
+        Controller4
     }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     void Update()
     {
-        
+        float moveX = Input.GetAxis("Horizontal " + ((int) input + 1));
+        float moveZ = Input.GetAxis("Vertical " + ((int) input + 1));
+
+        /*
+        if (moveX != 0)
+            Debug.Log(gameObject.name + " Input X: " + moveX);
+
+        if (moveZ != 0)
+            Debug.Log(gameObject.name + "  Input Z: " + moveZ);
+        */
+
+        //agent.destination = transform.position + new Vector3(moveX, 0, moveZ) * movementSpeed;
+        agent.Move(new Vector3(moveX, 0, moveZ) * movementSpeed * Time.deltaTime);
     }
 }
